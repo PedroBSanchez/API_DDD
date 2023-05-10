64a91a27589dca26279713f542ac61dbaf906a06
PedroBSanchez <pedro_b47@hotmail.com>
2023-05-09 21:42:43 -0300

    feature : Implementing Domain structure

---
+++ Domain/Services/ServiceCar.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Interfaces;
+using Domain.Interfaces.Generics;
+using Domain.Interfaces.InterfaceServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ServiceCar : IServiceCar
+    {
+
+        private readonly ICar _ICar;
+
+        public ServiceCar(ICar ICar)
+        {
+            _ICar = ICar;
+
+        }
+
+    }
+}
