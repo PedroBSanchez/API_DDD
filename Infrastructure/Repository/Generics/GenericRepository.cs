7aa0b11d0cd3b5ce4dd2c75c65ea78927f4b4703
PedroBSanchez <pedro_b47@hotmail.com>
2023-05-09 22:19:50 -0300

    feature : implementing infrastructure

---
+++ Infrastructure/Repository/Generics/GenericRepository.cs
@@ -0,0 +1,107 @@
+﻿using Domain.Interfaces.Generics;
+using Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Generics
+{
+    public class GenericRepository<T> : IGeneric<T>, IDisposable where T : class
+    {
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+
+        public GenericRepository()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task Add(T Object)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                await data.Set<T>().AddAsync(Object);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Update(T Object)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Update(Object);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T Object)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                data.Set<T>().Remove(Object);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task<T> GetEntityById(int Id)
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().FindAsync(Id);
+
+            }
+        }
+
+        public async Task<List<T>> List()
+        {
+            using (var data = new ContextBase(_OptionsBuilder))
+            {
+                return await data.Set<T>().ToListAsync();
+                
+            }
+        }
+
+
+
+
+
+
+
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~GenericRepository()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        
+    }
+}
